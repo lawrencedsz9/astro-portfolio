d6554f626bf918568b66c667452e4737b6ccfcb6
lawrencedsz9 <121675518+lawrencedsz9@users.noreply.github.com>
2023-04-24 10:34:32 +0530

    1

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+    {
+        name: "FOSS Website",
+        description: "A single page website on FOSS done with HTML & CSS",
+        link: "https://github.com/lawrencedsz9/foss",
+        stack: ["Check out the Source Code"],
+    },
+    {
+        name: "Campus Virtual Tour",
+        description: "A one stop platform developed as a team for an hackathon held in sjec",
+        link: "https://github.com/lawrencedsz9",
+        stack: ["Check out the Source Code"],
+
+        
+
+    },
+    {
+        name: "Portfolio-ASTRO",
+        description: "The Portfolio website developed with Astro",
+        link: "https://google.com",
+        stack: ["Check out the Source Code"],
+    },
+    {
+        name: "30-days-of-code",
+        description: "A repository of an event by sceptix-club",
+        link: "https://github.com/lawrencedsz9/sjec-30-days-of-code",
+        stack: ["Check out the Source Code"],
+    },
+    {
+        name: "work in progress",
+        description: "dont click me",
+        link: "https://www.youtube.com/shorts/SXHMnicI6Pg",
+        stack: [""],
+    },
+    {
+        name: "work in progress",
+        description: "dont click me",
+        link: "https://www.youtube.com/shorts/RG50Cm7TpVk",
+        stack: [""],
+    },
+];
