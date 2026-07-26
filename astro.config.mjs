bf1fa17b20d5be07ef7dae06d7f4f2a795a9369c
Lawrence <lawrencedsouza125@gmail.com>
2026-07-26 23:51:05 +0300

    updated UI with card refactor , visuals and animations for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,18 @@
 import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lawrencedsouza.netlify.app',
   output: 'server',
   adapter: netlify(),
-  integrations: [react()],
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
     }
   },
   vite: {
-    optimizeDeps: {
-      exclude: ['@splinetool/runtime']
-    },
     build: {
       chunkSizeWarningLimit: 2200
     }
